e5c6fb294bf99ba1e34f61883f0b0874d442e6ea
benmask90 <153077747+benmask90@users.noreply.github.com>
2023-12-06 20:41:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 
 const io = new Server(process.env.PORT || 9000, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://monumental-kangaroo-23b531.netlify.app',
     }, 
 })
 
